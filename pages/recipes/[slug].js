64b55d0191c0a8c213007756949f481bafeb3ccd
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-04-20 10:54:11 +0200

    Generating paths

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,3 +1,40 @@
-export default function RecipeDetails() {
+import { createClient } from "contentful";
+
+// Make conection to contentful space
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+// Fetch data from contentful for dynamic paths (slug)
+export async function getStaticPaths() {
+  const res = await client.getEntries({
+    content_type: "recipe",
+  });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+// Fetch data from contentful
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+
+  return {
+    props: { recipe: items[0] },
+  };
+}
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
   return <div>Recipe Details</div>;
 }
